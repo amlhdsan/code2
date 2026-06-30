a7e317d46b1d652912ad45dbae35060c2fe2055a
amlhdsan <amlhdsan@gmail.com>
2026-06-30 23:43:19 +0800

    upd

---
+++ sdlfjsdf.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while(ch < '0' || ch > '9') {
+        if(ch == '-') {
+            f = -1;
+        }
+        ch = getchar();
+    }
+    while(ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+
+inline void write(int x) {
+    if(x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if(x > 9)
+        write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+inline void writeln(int x) {
+    write(x);
+    putchar('\n');
+}
+
+int main() {
+    
+
+    return 0;
+}
