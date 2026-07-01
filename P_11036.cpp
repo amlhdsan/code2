85e90e58362e128fb9d79a55cebc53ff184bb720
amlhdsan <amlhdsan@gmail.com>
2026-07-01 22:51:09 +0800

    upd

---
+++ P_11036.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while(ch < '0' || ch > '9') {
+        if(ch == '-') {
+            f = -1;
+        }
+        ch = getchar();
+    }
+    while(ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+
+inline void write(int x) {
+    if(x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if(x > 9)
+        write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+inline void writeln(int x) {
+    write(x);
+    putchar('\n');
+}
+
+inline void solve() {
+    // int n;
+    n = read();
+}
+
+int main() {
+    int T = read();
+    while(T--) {
+        solve();
+    }
+    
+
+    return 0;
+}
