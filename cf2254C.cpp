a86e76d33976665690e739a7713ad808e8a2e1f6
amlhdsan <amlhdsan@gmail.com>
2026-08-07 00:41:16 +0800

    upd

--- cf2254C.cpp
+++ cf2254C.cpp
@@ -1,78 +1,66 @@
-/*
-*/
-
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-inline int read() {
-    int x = 0, f = 1;
-    char ch = getchar();
-    while(ch < '0' || ch > '9') {
-        if(ch == '-') {
-            f = -1;
-        }
-        ch = getchar();
+inline int read(){
+    int x=0,f=1;
+    char ch=getchar();
+    while(ch<'0'||ch>'9'){
+        if(ch=='-')f=-1;
+        ch=getchar();
     }
-    while(ch >= '0' && ch <= '9') {
-        x = (x << 3) + (x << 1) + (ch ^ 48);
-        ch = getchar();
+    while(ch>='0'&&ch<='9'){
+        x=(x<<3)+(x<<1)+(ch^48);
+        ch=getchar();
     }
-    return x * f;
+    return x*f;
 }
 
-inline void write(int x) {
-    if(x < 0) {
+inline void write(int x){
+    if(x<0){
         putchar('-');
-        x = -x;
+        x=-x;
     }
-    if(x > 9)
-        write(x / 10);
-    putchar(x % 10 + '0');
+    if(x>9)write(x/10);
+    putchar(x%10+'0');
 }
 
-inline void writeln(int x) {
+inline void writeln(int x){
     write(x);
     putchar('\n');
 }
 
+void solve(){
+    int n=read();
+    string s;
+    cin>>s;
 
+    int c[2]={0,0},b[2]={0,0};
 
-inline void solve() {
-    int n = read();
-    string a, b;
-    cin >> a >> b;
-
-    int ae = 0, ao = 0, be = 0, bo = 0;
-    for (int i = 0; i < n; ++i) {
-        if (a[i] == '1') {
-            if ((i & 1) == 0) ++ae; 
-            else ++ao;
-        }
-        if (b[i] == '1') {
-            if ((i & 1) == 0) ++be; 
-            else ++bo;
-        }
+    for(int i=0;i<n;i++){
+        int x=s[i]-'0';
+        c[x]++;
+        if(i==0||s[i-1]!=s[i])
+            b[x]++;
     }
 
-    if (ae == be && ao == bo) {
-        cout << "YES\n";
-    } else {
-        cout << "NO\n";
+    int d=c[0]-c[1];
+    if(abs(d)>2){
+        writeln(-1);
+        return;
     }
+
+    int ans=-1;
+    for(int x=-1;x<=1;x++){
+        if(abs(d-x)<=1)
+            ans=max(ans,b[0]+b[1]-abs(b[0]-b[1]-x));
+    }
+
+    writeln(n-ans);
 }
 
-int main() {
-
-    int t = read();
-
-    while(t--) {
+int main(){
+    int T=read();
+    while(T--)
         solve();
-    }
-    
-    if(t == 0) {
-        
-    }
-
     return 0;
 }
