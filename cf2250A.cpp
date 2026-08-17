c19ce23b58d58bcfb1e4626ef9a69375a58dc442
amlhdsan <amlhdsan@gmail.com>
2026-08-17 21:48:39 +0800

    upd

---
+++ cf2250A.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+#define N 1000010
+
+using namespace std;
+int a[N];
+
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while(ch < '0' || ch > '9') {
+        if(ch == '-') {
+            f = -1;
+        }
+        ch = getchar();
+    }
+    while(ch >= '0' && ch <= '9') {
+        x = (x << 3) + (x << 1) + (ch ^ 48);
+        ch = getchar();
+    }
+    return x * f;
+}
+
+inline void write(int x) {
+    if(x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if(x > 9)
+        write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+inline void writeln(int x) {
+    write(x);
+    putchar('\n');
+}
+
+inline void solve() {
+    int n = read();
+    for(int i = 1; i <= n; ++i) {
+        a[i] = read();
+    }
+
+    if(n & 1) {
+        puts("NO");
+        return;
+    }
+
+    int minn = 1e9, maxn = -1e9;
+    for(int i = 1; i <= n; i += 2) {
+        minn = min(minn, a[i]);
+    }
+
+    for(int i = 2; i <= n; i += 2) {
+        maxn = max(maxn, a[i]);
+    }
+
+    if(minn > maxn) {
+        puts("YES");
+    } else {
+        puts("NO");
+    }
+}
+
+int main() {
+    int t = read();
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
